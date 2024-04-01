11cdb09ff351f5fee77e3726864afe8f0aebcc8b
hafeezBaluch <hafeezullahhassan2019.com>
2024-04-01 09:57:51 +0500

    Implement new design theme for the URL page

--- scrape/urls.py
+++ scrape/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('scrape-page/', views.scrape_web_page, name='scrape_web_page'),
+    path('', views.scrape_web_page, name='scrape_web_page'),
     path('scraped-text/<str:url>/', views.scrape_web_page, name='scraped_text'),
 
 ]
